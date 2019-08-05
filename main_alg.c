583d4ea3ea5cab81a117973a9176f2155ef7b046
hlarson <aa-badalov1@mail.ru>
2019-08-05 13:03:09 +0300

    asd

---
+++ main_alg.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_alg.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlarson <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/05 12:26:15 by hlarson           #+#    #+#             */
+/*   Updated: 2019/08/05 13:02:57 by hlarson          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+void		get_graph_len(t_graph **graph, size_t *start, size_t *end, size_t *len)
+{
+	*len = 0;
+	while (graph[*len])
+	{
+		if (graph[*len]->start == 1)
+			*start_num = *len;
+		if (graph[*len]->end == 1)
+			*end_num = *len;
+		*len++;
+	}
+}
+
+void		main_alg(t_graph **graph)
+{
+	size_t	start;
+	size_t	end;
+	size_t	len;
+	int		*queue;
+
+	get_graph_param(graph, &start, &end, &len);
+	queue = (int *)malloc(sizeof(int) * len);
+
+
