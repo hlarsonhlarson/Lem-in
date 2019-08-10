7f752c4d536e73b4a9dd8d16d44ee5431a554ccc
hlarson <aa-badalov1@mail.ru>
2019-08-10 20:36:08 +0300

    zxc

--- main_alg.c
+++ main_alg.c
@@ -6,7 +6,7 @@
 /*   By: hlarson <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/05 12:26:15 by hlarson           #+#    #+#             */
-/*   Updated: 2019/08/08 19:29:32 by hlarson          ###   ########.fr       */
+/*   Updated: 2019/08/10 17:44:52 by hlarson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,6 @@ t_adjacency		*get_way(int *path, int u)
 	adj->node_num = u;
 	adj->next = NULL;
 	tmp = adj;
-	//tmp = adj->next;
 	u = path[u];
 	while (u != path[u])
 	{
@@ -82,9 +81,9 @@ t_adjacency		*get_way(int *path, int u)
 		adj = adj->next;
 		u = path[u];
 	}
-    adj->next = (t_adjacency *)malloc(sizeof(t_adjacency));
-    adj->next->node_num = u;
-    adj->next->next = NULL;
+	adj->next = (t_adjacency *)malloc(sizeof(t_adjacency));
+	adj->next->node_num = u;
+	adj->next->next = NULL;
 	return (tmp);
 }
 
@@ -108,7 +107,7 @@ void		ft_del_path(t_adjacency **head_path)
 {
 	t_adjacency	*tmp;
 	t_adjacency	*swap;
-		
+
 	tmp = *head_path;
 	while (tmp)
 	{
@@ -118,7 +117,7 @@ void		ft_del_path(t_adjacency **head_path)
 	}
 }
 
-void        recreate_graph(t_graph **graph, int *path_num, int start, int end)
+t_adjacency		*recreate_graph(t_graph **graph, int *path_num, int start, int end)
 {
 	t_adjacency	*path;
 	t_adjacency	*tmp_graph;
@@ -128,59 +127,53 @@ void        recreate_graph(t_graph **graph, int *path_num, int start, int end)
 	head_path = path;
 	while (path)
 	{
-	    if (path->next == NULL)
-        {
-	        ft_del_path(&head_path);
-	        return ;
-        }
-	   	tmp_graph = graph[path->next->node_num]->adjacency;
+		if (path->next == NULL)
+			return (head_path);
+		tmp_graph = graph[path->next->node_num]->adjacency;
 		while (tmp_graph)
 		{
-		    if (tmp_graph->node_num == path->node_num)
-		        tmp_graph->node_num = start;
+			if (tmp_graph->node_num == path->node_num)
+				tmp_graph->node_num = start;
 			tmp_graph = tmp_graph->next;
 		}
 		path = path->next;
 	}
 }
 
-void        print_way(int *path, int u, t_graph **graph)
+void        print_way(t_adjacency *path, t_graph **graph)
 {
-    if (path[u] != u)
-        print_way(path, path[u], graph);
-    ft_printf("%s\n", graph[u]->name);
+	while (path)
+	{
+		ft_printf("%s\n", graph[path->node_num]->name);
+		path = path->next;
+	}
 }
 
 void        help_fun(t_graph **graph, size_t start, size_t end, size_t len)
 {
 	t_queue *queue;
-	int     **path;
-    int  path_num;
+	t_adjacency	**path;
+	int  path_num;
 
 	queue = init_queue(len);
 	push_queue(queue, (int)start);
 	path_num = count_path(graph, start);
-	path = (int **)malloc(sizeof(int) * (int)path_num);
+	path = (t_adjacency **)malloc(sizeof(t_adjacency) * (int)path_num);
 	while (path_num != -1)
 	{
-		path[path_num] = get_path(queue, graph, start, end);
-        recreate_graph(graph, path[path_num], start, end);
-        free(queue->elements);
-        free(queue);
-        queue = init_queue(len);
-        push_queue(queue, (int)start);
+		path[path_num] = recreate_graph(graph,
+				get_path(queue, graph, start, end), start, end);
+		free(queue->elements);
+		free(queue);
+		queue = init_queue(len);
+		push_queue(queue, (int)start);
 		path_num--;
 	}
-	//print_way(path, graph, (int)end);
 	if (path == NULL)
 		return ;
-	print_way(path[0], end, graph);
+	print_way(path[0], graph);
 	ft_printf("HI\n");
-    print_way(path[1], end, graph);
-	free(path);
-	free(queue->elements);
-	free(queue);
-
+	print_way(path[1], graph);
 }
 
 void		main_alg(t_graph **graph)
